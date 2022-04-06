74843c515470454cea34f7d31c4091751106fb8b
noiseLim <noiselim@gmail.com>
2022-04-06 20:19:48 +0300

    fixed eslint errors

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const webpack = require('webpack');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
