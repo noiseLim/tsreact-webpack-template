e3d4bdf0f9b43762752393fe9434a1d351b40a6a
noiseLim <noiselim@gmail.com>
2022-04-17 13:05:38 +0300

    updated to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import ErrorBoundry from './components/ErrorBoundary';
 
 import './styles/style.scss';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(
   <ErrorBoundry>
     <App />
-  </ErrorBoundry>,
-  document.getElementById('root')
+  </ErrorBoundry>
 );
