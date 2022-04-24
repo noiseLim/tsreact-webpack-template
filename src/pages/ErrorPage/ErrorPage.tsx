15dbf341bbd6f1ea9288d0face3bc5465ee39a5c
noiseLim <noiselim@gmail.com>
2022-04-24 19:54:26 +0300

    changed errorPage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,30 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
+import { AlertTitle, Alert, Button } from '@mui/material';
+import './errorPage.scss';
 
 const ErrorPage: FC = () => {
-  return <div>Oops, something went wrong...</div>;
+  const onReloadPage = () => window.location.reload();
+
+  return (
+    <div className='errorMessage'>
+      <Alert severity='error' className='errorMessage__title'>
+        <AlertTitle>
+          <strong>Oh-no! Something went wrong</strong>
+        </AlertTitle>
+        You can refresh the page or try again later.
+        <br />
+        <Button
+          className='errorMessage__btn'
+          variant='outlined'
+          color='error'
+          value='Reload Page'
+          onClick={onReloadPage}
+        >
+          Reload the page
+        </Button>
+      </Alert>
+    </div>
+  );
 };
 
 export default ErrorPage;
