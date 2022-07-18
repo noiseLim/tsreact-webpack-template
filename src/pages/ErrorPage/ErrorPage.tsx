503e382355559037c47cc417dd3dbbc5ec16e236
noiseLim <noiselim@gmail.com>
2022-07-18 21:47:15 +0300

    fixed styles in ErrorPage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -6,15 +6,17 @@ const ErrorPage: FC = () => {
   const onReloadPage = () => window.location.reload();
 
   return (
-    <div className='errorMessage'>
-      <Alert severity='error' className='errorMessage__title'>
+    <div className='error-message'>
+      <Alert severity='error' className='error-message__title'>
         <AlertTitle>
           <strong>Oh-no! Something went wrong</strong>
         </AlertTitle>
         You can refresh the page or try again later.
         <br />
         <Button
-          className='errorMessage__btn'
+          sx={{
+            marginTop: '10px',
+          }}
           variant='outlined'
           color='error'
           value='Reload Page'
