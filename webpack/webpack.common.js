696294f46c9cbca85c25c2eec9ac1cb474c5573c
noiseLim <noiselim@gmail.com>
2023-02-04 11:41:53 +0530

    small improvements for webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  entry: path.resolve(__dirname, '..', 'src', 'index.tsx'),
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
@@ -42,12 +42,13 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, '..', './build'),
+    path: path.resolve(__dirname, '..', 'build'),
     filename: 'bundle.js',
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './src/index.html'),
+      template: path.resolve(__dirname, '..', 'src', 'index.html'),
       favicon: './public/favicon.png',
     }),
   ],
